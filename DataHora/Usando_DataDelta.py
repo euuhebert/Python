6a4de560dd57cd7a7b159f0a1986e6a5cf2aba25
Hebert <hebertsrocha79@gmail.com>
2024-06-29 16:56:43 -0300

    add black e isort no ambiente

--- DataHora/Usando_DataDelta.py
+++ DataHora/Usando_DataDelta.py
@@ -1,43 +1,34 @@
 from datetime import date, datetime, timedelta
 
-
 # Variáveis de tempo para cada tipo de carro
-tempos = {
-    'P': 30,
-    'M': 60,
-    'G': 90
-}
+tempos = {"P": 30, "M": 60, "G": 90}
 
-tipo_carro = input('Selcione o tipo de carro [P], [M], [G]: ').strip().upper()
+tipo_carro = input("Selcione o tipo de carro [P], [M], [G]: ").strip().upper()
 
 data_atual = datetime.now()
 
 match tipo_carro:
-    case 'P':
-        tempo = tempos['P']
+    case "P":
+        tempo = tempos["P"]
         data_estimada = data_atual + timedelta(minutes=tempo)
         print(f"O carro chegou {data_atual} e ficará pronto {data_estimada}")
-    case 'M':
-        tempo = tempos['M']
+    case "M":
+        tempo = tempos["M"]
         data_estimada = data_atual + timedelta(minutes=tempo)
         print(f"O carro chegou {data_atual} e ficará pronto {data_estimada}")
-    case 'G':
-        tempo = tempos['G']
+    case "G":
+        tempo = tempos["G"]
         data_estimada = data_atual + timedelta(minutes=tempo)
         print(f"O carro chegou {data_atual} e ficará pronto {data_estimada}")
     case _:
         print("Tipo de carro não reconhecido")
 
 
-#Versão de código refatorada
+# Versão de código refatorada
 
-tempos = {
-    'P': 30,
-    'M': 60,
-    'G': 90
-}
+tempos = {"P": 30, "M": 60, "G": 90}
 
-tipo_carro = input('Selecione o tipo de carro [P], [M], [G]: ').strip().upper()
+tipo_carro = input("Selecione o tipo de carro [P], [M], [G]: ").strip().upper()
 
 data_atual = datetime.now()
 
@@ -48,9 +39,3 @@ if tempo is not None:
     print(f"O carro chegou {data_atual} e ficará pronto {data_estimada}")
 else:
     print("Tipo de carro não reconhecido")
-    
-
-
-
-
-
