64c023e9c977f5fe9214b3cf058bef1fbd1ef2dd
Hebert <hebertsrocha79@gmail.com>
2024-06-29 12:59:47 -0300

    concluido o modulo

--- DataHora/Definindo_zona.py
+++ DataHora/Definindo_zona.py
@@ -1,5 +1,11 @@
 import pytz
+from datetime import datetime
 
-print('a')
-print('b')
-print('c')
+#Usando a biblioteca pytz
+data = datetime.now(pytz.timezone('Europe/Oslo'))
+data2 = datetime.now(pytz.timezone('America/Sao_Paulo'))
+
+print(data)
+print(data2)
+
+
